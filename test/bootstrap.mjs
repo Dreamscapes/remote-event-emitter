29f1d537dbb8c3ed189dab635f2bb9ce01a3c38a
Robert Rossmann <rr.rossmann@me.com>
2018-11-13 18:23:32 +0100

    chore: add additional dev deps

--- test/bootstrap.mjs
+++ test/bootstrap.mjs
@@ -1,5 +1,12 @@
-import { expect } from 'chai'
+import chai from 'chai'
 import sinon from 'sinon'
+import sinonChai from 'sinon-chai'
 
-global.expect = expect
+// Make sure our tests always run in the 'test' environment
+// eslint-disable-next-line no-process-env
+process.env.NODE_ENV = 'test'
+
+global.expect = chai.expect
 global.sinon = sinon
+
+chai.use(sinonChai)
